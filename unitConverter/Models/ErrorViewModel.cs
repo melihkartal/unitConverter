a88066368812167453aa80f0910daf4a3654daa6
melih <86454129+melihkartal@users.noreply.github.com>
2024-09-21 22:30:26 -0400

    created a new mvc app

---
+++ unitConverter/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace unitConverter.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
